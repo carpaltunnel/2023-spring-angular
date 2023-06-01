aefbaa2fe93a59005ee5466d9d2058c8046715d6
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-05-31 19:57:02 -0500

    mock

---
+++ src/app/mock-vehicles.ts
@@ -0,0 +1,22 @@
+import { Vehicle } from "./vehicle";
+
+export const VEHICLES: Vehicle[] = [
+  {
+    id: 'a2a69a22-68c8-4ce5-bd45-3fda93b97896',
+    make: 'Honda',
+    model: 'Accord',
+    year: 2020,
+  },
+  {
+    id: '7c104bd2-0c6b-47ba-8225-8dbf24c0b538',
+    make: 'Toyota',
+    model: 'Avalon',
+    year: 2016,
+  },
+  {
+    id: '1574d423-377f-478e-9f72-653fce2e5f0a',
+    make: 'Ford',
+    model: 'Raptor',
+    year: 2023,
+  }
+];
