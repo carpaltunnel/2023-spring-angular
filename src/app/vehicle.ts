1bf66c35303ef194d2fd451f146444823b3c286d
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-05-31 19:14:18 -0500

    v

---
+++ src/app/vehicle.ts
@@ -0,0 +1,7 @@
+export interface Vehicle {
+  id: string;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+}
--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Vehicle } from '../vehicle';
 
 @Component({
   selector: 'app-vehicles',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./vehicles.component.css']
 })
 export class VehiclesComponent {
-  make = 'Honda';
+  vehicle: Vehicle = {
+    id: 'a2a69a22-68c8-4ce5-bd45-3fda93b97896',
+    make: 'Honda',
+    model: 'Accord',
+    year: 2020,
+    color: 'blue',
+  };
 }
