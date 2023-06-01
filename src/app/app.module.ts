7130cc34fd10e7fb697ce50cf3a5b421b74328dd
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-05-31 19:34:09 -0500

    v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { VehiclesComponent } from './vehicles/vehicles.component';
     VehiclesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
