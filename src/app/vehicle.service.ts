af4c6d3637cedbdb02c7aedf57a90b6eb4543dae
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-06-05 20:15:36 -0500

    message

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { Vehicle } from './vehicle';
 import { VEHICLES } from './mock-vehicles';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getVehicles(): Observable<Vehicle[]> {
     const vehicles = of(VEHICLES);
+    this.messageService.add('VehicleService: getVehicles()');
     return vehicles;
   }
 }
