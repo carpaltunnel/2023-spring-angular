fe4fb88b1e97d93e200e33e3370799e657652cef
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-06-07 18:30:51 -0500

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
-
+const routes: Routes = [
+  { path: 'vehicles', component: VehiclesComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
