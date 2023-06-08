c0c757810ec1db3485f0744fde4174c260e13bb6
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2023-06-07 19:20:33 -0500

    vdetails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'vehicles', component: VehiclesComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'vehicles/:id', component: VehicleDetailsComponent }
 ];
 
 @NgModule({
